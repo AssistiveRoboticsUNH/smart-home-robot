269adcd44745bf36c02044b6a8e89dc6f678c603
mnakash <akashmoniruzzaman@gmail.com>
2025-11-05 23:27:38 -0500

    bugfix: add helper tf yaml for docking port and charger transform

--- yaml_tf_broadcaster/launch/tf_broadcast_olson.launch.py
+++ yaml_tf_broadcaster/launch/tf_broadcast_olson.launch.py
@@ -3,20 +3,21 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
-
     ld = LaunchDescription()
 
     pkg_path = get_package_share_directory('yaml_tf_broadcaster') + "/config/"
 
     room_file = DeclareLaunchArgument(
         "rooms_location",
-        default_value=pkg_path + "gaskin_tf.yaml",
+        default_value=pkg_path + "olson_rooms.yaml",
         description="rooms location"
     )
     ld.add_action(room_file)
-    
+
     rooms = Node(
         package="yaml_tf_broadcaster",
         executable="yaml_broadcaster_node",
@@ -26,24 +27,5 @@ def generate_launch_description():
         ]
     )
     ld.add_action(rooms)
-
-    helper_file = DeclareLaunchArgument(
-        "helper_location",
-        default_value=pkg_path + "helpers.yaml",
-        description="helper location"
-    )
-
-    ld.add_action(helper_file)
-
-    helpers = Node(
-        package="yaml_tf_broadcaster",
-        executable="yaml_broadcaster_node",
-        name="helper_launch_ll",
-        parameters=[
-            {"yaml_file_name": LaunchConfiguration("helper_location")}
-        ]
-    )
-
-    ld.add_action(helpers)
 
     return ld
